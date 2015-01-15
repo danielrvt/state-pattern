8c750ca0f2097a40c4cf70b4e8a435f7f45dc195
Daniel Rodriguez del Villar Trimarchi <daniel.rodriguez@synergy-gb.com>
2015-01-15 17:30:34 -0430

    Agregada interfaz para los estados

---
+++ src/main/java/statemachine/Lock.java
@@ -0,0 +1,16 @@
+package statemachine;
+
+import statemachine.states.State;
+
+/**
+ * Created by budi on 15/01/15.
+ */
+public class Lock {
+    State currentState;
+
+    // Transición 1
+
+    //...
+
+    // Transición n
+}
---
+++ src/main/java/statemachine/states/State.java
@@ -0,0 +1,10 @@
+package statemachine.states;
+
+/**
+ * Created by budi on 15/01/15.
+ * <p/>
+ */
+public interface State {
+
+
+}
